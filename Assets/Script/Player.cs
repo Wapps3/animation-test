c6ecbdb1c27a742a5bfe25c97c6a0bec3229284b
Wapps3 <wapps3@gmail.com>
2019-12-10 17:36:09 +0100

    Add punch,run, fire fist

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject prefab;
+
+    Animator animator;
+
+    public float speed = 10;
+
+    public float damage = 10.0f;
+
+    public bool fire = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Speed", 0);
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            animator.SetTrigger("Attack");
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetTrigger("Cast");
+            fire = true;
+            GameObject tmp = GameObject.Find("Mutant:RightHand").gameObject;
+
+            Instantiate(prefab, tmp.transform.position + new Vector3(0, 0.1f, 0), Quaternion.identity, tmp.transform);
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                gameObject.transform.Rotate(.0f, 90.0f * Time.deltaTime, 0.0f, Space.World);
+            }
+            if (Input.GetKey(KeyCode.Q))
+            {
+                gameObject.transform.Rotate(.0f, -90.0f * Time.deltaTime, 0.0f, Space.World);
+            }
+            if (Input.GetKey(KeyCode.Z))
+            {
+                animator.SetFloat("Speed", Time.deltaTime * speed);
+                gameObject.transform.position += gameObject.transform.forward * Time.deltaTime * speed;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                animator.SetFloat("Speed", Time.deltaTime * speed);
+                gameObject.transform.position += gameObject.transform.forward * -1 * Time.deltaTime * speed;
+            }
+        }
+
+       
+    }
+}
---
+++ Assets/Script/Wall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public float life = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (life <= 0)
+           Object.Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponentInParent<Player>().fire)
+            life -= 4*other.GetComponentInParent<Player>().damage;
+        else
+            life -= other.GetComponentInParent<Player>().damage;
+
+        Debug.Log("Life: " + life);
+    }
+}
